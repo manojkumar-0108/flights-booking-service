0070865b7f8b15f21af988e98ef29069eaa7b3b7
Manoj Kumar <manoj8894421801@gmail.com>
2024-04-04 08:41:29 +0530

    FIX: Identity Column value gaps solved by IDENTITY RESET

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 
-const { ServerConfig } = require("./config");
-
+const { ServerConfig, Logger } = require("./config");
+const { IdentityReset } = require("./utils/helpers/");
+const { sequelize } = require('./models');
 const apiRoutes = require("./routes");
 const errorHandler = require("./utils/error.handler");
 
@@ -15,4 +16,20 @@ app.use(errorHandler);
 
 app.listen(ServerConfig.PORT, () => {
     console.log(`Started server at PORT: ${ServerConfig.PORT}`);
+
+    /**
+ * Resetting Identity column
+ */
+    sequelize.authenticate()
+        .then(() => {
+            return IdentityReset();
+        })
+        .then(() => {
+            console.log("Succes: Identity seed reset successfull");
+        })
+        .catch(error => {
+            console.log('Identity seed reset -- failed -- for all models');
+            console.error('Database is not connected:', error);
+            Logger.error({ message: "Database is not Connected!!!", error: error });
+        });
 })
